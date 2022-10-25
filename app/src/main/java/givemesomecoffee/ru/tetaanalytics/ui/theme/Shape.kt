57ec5c99295eabf667f202449cf409ae67dc1c8b
givemesomecoffee <givemesomecoffee@yandex.ru>
2022-10-25 18:16:01 +0300

    init

---
+++ app/src/main/java/givemesomecoffee/ru/tetaanalytics/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package givemesomecoffee.ru.tetaanalytics.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
